c84cffc9c5d65f99d798e671b6ab6fb2a8ee69ec
JesterJoyless <thepalerideris@gmail.com>
2023-06-05 12:11:11 +0500

    Initial Commit

--- src/test/java/guru/springframework/msscbreweryclient/MsscBreweryClientApplicationTests.java
+++ src/test/java/guru/springframework/msscbreweryclient/MsscBreweryClientApplicationTests.java
@@ -1,10 +1,22 @@
 package guru.springframework.msscbreweryclient;
 
+import guru.springframework.msscbreweryclient.web.client.BreweryClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
 
 @SpringBootTest
 public class MsscBreweryClientApplicationTests {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void testDeleteBeer(){
+        client.deleteBeer(UUID.randomUUID());
+    }
 
     @Test
     public void contextLoads() {
